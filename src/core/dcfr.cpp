a969d49e539faac5d348dcb75a3934cd8a4e609d
Wellsaik <artem.podorozhko@outlook.com>
2026-06-25 10:59:43 +0300

    compile fix of dcfr

--- src/core/dcfr.cpp
+++ src/core/dcfr.cpp
@@ -1,26 +1,6 @@
 #include "core/dcfr.hpp"
 
 namespace core {
-
-template <class G>
-DCFRSolver<G>::DCFRSolver(DCFRConfig config) : config_(config) {}
-
-template <class G>
-void DCFRSolver<G>::set_locked_strategies(
-    std::unordered_map<InfosetKey, std::vector<Probability>> locked) {
-    locked_ = std::move(locked);
+// Template implementations live in dcfr.hpp so concrete game specializations
+// can instantiate them from any translation unit.
 }
-
-template <class G>
-SolveOutput DCFRSolver<G>::solve(std::uint32_t iterations) {
-    (void)config_;
-    (void)locked_;
-    SolveOutput out;
-    out.iterations = iterations;
-    return out;
-}
-
-// Explicit instantiation will be added once concrete game types exist.
-
-}  // namespace core
-
