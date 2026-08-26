87789f28b4ec46e3466e12906d181520310a0e8e
Wellsaik <artem.podorozhko@outlook.com>
2026-08-26 14:08:37 +0300

    refactor(namespace): reduce multiway model imports

--- include/solver/multiway_blueprint_trainer.hpp
+++ include/solver/multiway_blueprint_trainer.hpp
@@ -1,7 +1,5 @@
 #pragma once
 
-#include "core/legacy_namespace_compat.hpp"
-
 #include "solver/multiway_export.hpp"
 #include "solver/multiway_artifact.hpp"
 #include "solver/multiway_traversal.hpp"
